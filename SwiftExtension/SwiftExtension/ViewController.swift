7513eb08448c9086322b76e5d94fecc896e0d0c1
QiMeng <616684889@qq.com>
2014-06-13 14:14:29 +0800

    swift扩展

---
+++ SwiftExtension/SwiftExtension/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftExtension
+//
+//  Created by strongsoft on 14-6-13.
+//  Copyright (c) 2014年 QiMeng_LYS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
